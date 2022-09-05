a48c4fdbb3b55b098ae0703da58e53a600e4a103
Kirill Sokolov <kirill.sokolov.work@gmail.com>
2022-09-05 22:32:27 +0300

    add bot token

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ namespace Ventil_Telegram_Bot
     {
         static void Main(string[] args)
         {
-            var botClient = new TelegramBotClient("");
+            var botClient = new TelegramBotClient("5634522332:AAFFl54M7o9KJaOLtufTRja0ekH7iR9rAKQ");
         }
     }
 }
